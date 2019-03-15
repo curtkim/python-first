de20cdaf654df9db29574ab0e2d6eaeb0af03a5d
curtkim <iamteri@gmail.com>
2019-03-15 21:32:11 +0900

    struct, avro nested record schema 예제추가

---
+++ test_fastavro2.py
@@ -0,0 +1,55 @@
+from fastavro import writer, reader, parse_schema
+import fastavro
+from fastavro.six import MemoryIO
+
+
+schema = {
+    'name': 'Call',
+    'type': 'record',
+    'fields': [
+        {"name": "id", "type": "long"},
+        {"name": "origin", "type":
+            {"type": "record", "name": "Location",
+             "fields": [
+                 {"name": "name", "type": "string"},
+                 {"name": "lat", "type": "double"},
+                 {"name": "lng", "type": "double"},
+                 {"name": "region_code", "type": ["null", "int"]},
+             ]
+             }
+         },
+        {"name": "destination", "type": "Location"},
+    ],
+}
+
+parsed_schema = parse_schema(schema)
+
+records = [
+    {
+        'id': 1,
+        'origin': {'name': '강남역', 'lat': 37, 'lng': 127, 'region_code': 1234567},
+        'destination': {'name': '서울역', 'lat': 37.1, 'lng': 127.1, 'region_code': 1234568}
+    }
+]
+
+
+def test_write_read():
+    new_file = MemoryIO()
+    writer(new_file, parsed_schema, records)
+    new_file.seek(0)
+    new_records = list(reader(new_file, parsed_schema))
+
+    assert new_records == records
+
+
+def test_schemaless_write_read():
+    new_file = MemoryIO()
+    fastavro.schemaless_writer(new_file, parsed_schema, records[0])
+    new_file.seek(0)
+
+    # bytes로 변환?
+    new_file.seek(0)
+
+    new_record = fastavro.schemaless_reader(new_file, parsed_schema)
+    assert records[0] == new_record
+
---
+++ test_struct.py
@@ -0,0 +1,13 @@
+import struct
+
+
+def test_pack_unpack():
+    """
+        > : big-endian
+        h : short(2 bytes)
+        l : long(4 bytes)
+        from https://docs.python.org/2/library/struct.html
+    """
+    assert isinstance(struct.pack('>hhl', 1, 2, 3), bytes)
+    assert b'\x00\x01\x00\x02\x00\x00\x00\x03' == struct.pack('>hhl', 1, 2, 3)
+    assert (1, 2, 3) == struct.unpack('>hhl', b'\x00\x01\x00\x02\x00\x00\x00\x03')

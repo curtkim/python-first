3c0eb1620884492091eda1a202b60907b395bbc4
curt.k <curt.k@kakaocorp.com>
2019-12-27 18:11:16 +0900

    chess수정. 에러있음

--- rxpy-first/chess/chess.py
+++ rxpy-first/chess/chess.py
@@ -4,6 +4,7 @@ from os.path import dirname, join
 
 import pygame
 
+import rx
 from rx import operators as ops
 from rx.subject import Subject
 from rx.scheduler.mainloop import PyGameScheduler
@@ -35,39 +36,43 @@ def main():
     ]]
     images = [pygame.image.load(image).convert_alpha() for image in files]
 
-    def accumulator(acc, ev):
-        new_arr = []
-        for i, rect in enumerate(acc):
-            new_rect = rect.copy()
-            new_rect.top = ev[1]
-            new_rect.left = ev[0] + i * 32
-            new_arr.append(new_rect)
-        return new_arr
+    scheduler = PyGameScheduler(pygame)
+    mousemove = Subject()
+
+    def convert_rec(origin, ev, idx):
+        (old_idx, old_rect) = origin
+        new_rect = old_rect.copy()
+        new_rect.top = ev[1]
+        new_rect.left = ev[0] + idx * 32
+        return (idx, new_rect)
+
+    def image2update(image, idx):
+        mousemove.pipe(
+            ops.delay(0.1 * idx, scheduler=scheduler),
+            ops.scan(lambda origin, ev: convert_rec(origin, ev, idx), (idx, images[idx].get_rect())),
+            ops.pairwise()
+        )
 
     def on_error(err):
         print("Got error: %s" % err)
         sys.exit()
 
-    def render(arr_pair):
-        (erase, draw) = arr_pair #tuple destruction
-        for rect in erase:
+    def render(arr):
+        for item in arr:
+            (idx, rect) = item[0]
             screen.blit(background, (rect.x, rect.y), rect)
-
-        for idx, rect in enumerate(draw):
+            (idx, rect) = item[1]
             screen.blit(images[idx], rect)
 
-        pygame.display.update(erase+draw)
+        list = [item[0].rect for item in arr] + [item[1].rect for item in arr]
+        pygame.display.update(list)
         pygame.display.flip()
 
-    scheduler = PyGameScheduler(pygame)
-    mousemove = Subject()
-
-    mousemove.pipe(
-        #ops.delay(0.1 * i, scheduler=scheduler),
-        ops.scan(accumulator, [image.get_rect() for image in images]),
-        ops.pairwise(),
-        ops.observe_on(scheduler)
-    ).subscribe(render, on_error)
+    rx.from_(images).pipe(
+        ops.flat_map_indexed(image2update),
+        ops.merge_all(),
+        ops.buffer(rx.interval(1.0/30))
+    ).subscribe(render)
 
     while True:
         for event in pygame.event.get():

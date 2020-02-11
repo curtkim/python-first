abc95bec66308980a5e8b090be176c7048dbba48
curt.k <curt.k@kakaocorp.com>
2020-02-11 16:10:26 +0900

    hydra-first추가

---
+++ hydra-first/my_log.py
@@ -0,0 +1,19 @@
+import hydra
+
+import logging
+# A logger for this file
+log = logging.getLogger(__name__)
+
+@hydra.main()
+def my_log(_cfg):
+    log.info("Info level message")
+    log.debug("Debug level message")
+
+if __name__ == "__main__":
+    my_log()
+
+'''
+$ python my_log.py hydra.verbose=__main__
+[2020-02-11 16:09:52,431][__main__][INFO] - Info level message
+[2020-02-11 16:09:52,431][__main__][DEBUG] - Debug level message
+'''
---
+++ hydra-first/step2/my_app2.py
@@ -0,0 +1,32 @@
+import hydra
+from omegaconf import DictConfig
+
+@hydra.main(config_path="conf")
+def my_app(cfg : DictConfig) -> None:
+    print(cfg.pretty())
+
+if __name__ == "__main__":
+    my_app()
+
+'''
+$ python my_app.py
+{}
+
+
+$ python my_app2.py db=postgres
+db:
+  driver: postgresql
+  pass: drowssap
+  timeout: 10
+  user: postgre_user
+
+
+$ python my_app2.py db=postgres db.timeout=20
+db:
+  driver: postgresql
+  pass: drowssap
+  timeout: 20
+  user: postgre_user
+
+
+'''

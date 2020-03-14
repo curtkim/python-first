9065e980d7739d08e15ddf6f5759118c014831fc
curt <iamteri@gmail.com>
2020-03-14 21:43:16 +0900

    view_pointcloud.py에서 Example class제거

--- moderngl-first/view_pointcloud.py
+++ moderngl-first/view_pointcloud.py
@@ -34,22 +34,6 @@ import moderngl_window as mglw
 import open3d as o3d
 
 
-class Example(mglw.WindowConfig):
-    gl_version = (3, 3)
-    title = "ModernGL Example"
-    window_size = (1280, 720)
-    aspect_ratio = 16 / 9
-    resizable = True
-    samples = 4
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    @classmethod
-    def run(cls):
-        mglw.run_window_config(cls)
-
-
 
 class Camera():
 
@@ -131,8 +115,12 @@ class Camera():
             self._z_far)
 
 
-class PerspectiveProjection(Example):
+class PerspectiveProjection(mglw.WindowConfig):
     gl_version = (3, 3)
+    window_size = (1280, 720)
+    aspect_ratio = 16 / 9
+    resizable = True
+    samples = 4
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -240,4 +228,4 @@ class PerspectiveProjection(Example):
 
 
 if __name__ == '__main__':
-    PerspectiveProjection.run()
+    mglw.run_window_config(PerspectiveProjection)

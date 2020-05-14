06e6545c6916fd236114ba570cbb0db96527c56e
curt <iamteri@gmail.com>
2020-05-14 10:43:36 +0900

    XVIZGLBWriter관련 추가

--- xviz-avs-first/first.py
+++ xviz-avs-first/first.py
@@ -3,12 +3,9 @@ from circle import CircleScenario
 
 scenario = CircleScenario()
 
-#print(scenario.get_metadata())
-#print(scenario.get_message(0))
-
-source = xi.DirectorySource("output")
-writer = xi.XVIZJsonWriter(source)
-writer.write_message(scenario.get_metadata_inner())
-writer.write_message(scenario.get_message_inner(0))
-writer.write_message(scenario.get_message_inner(1))
-writer.close()
+#, xi.XVIZGLBWriter(xi.DirectorySource("output_glb"))
+for writer in [xi.XVIZJsonWriter(xi.DirectorySource("output"))]:
+    writer.write_message(scenario.get_metadata_inner())
+    writer.write_message(scenario.get_message_inner(0))
+    writer.write_message(scenario.get_message_inner(1))
+    writer.close()

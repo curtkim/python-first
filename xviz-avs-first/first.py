2a5d2ecf139d108561c6d4ce6aece7da54e625bc
curt.k <curt.k@kakaocorp.com>
2020-05-26 15:18:19 +0900

    xviz-avs-first 수정

--- xviz-avs-first/first.py
+++ xviz-avs-first/first.py
@@ -1,11 +1,12 @@
 import xviz_avs.io as xi
 from circle import CircleScenario
 
-scenario = CircleScenario()
+scenario = CircleScenario(live=False)
 
 #, xi.XVIZGLBWriter(xi.DirectorySource("output_glb"))
-for writer in [xi.XVIZJsonWriter(xi.DirectorySource("output"))]:
-    writer.write_message(scenario.get_metadata_inner())
-    writer.write_message(scenario.get_message_inner(0))
-    writer.write_message(scenario.get_message_inner(1))
-    writer.close()
+writer = xi.XVIZJsonWriter(xi.DirectorySource("output"))
+writer.write_message(scenario.get_metadata_inner())
+writer.write_message(scenario.get_message_inner(0))
+writer.write_message(scenario.get_message_inner(1))
+writer.close()
+
---
+++ xviz-avs-first/temp.py
@@ -0,0 +1,28 @@
+import json
+
+def extract_start_end(file_name):
+    with open(file_name) as json_file:
+        body = json.load(json_file)
+        print(body['messages'].keys())
+
+        keys = list(body['messages'].keys())
+        start = keys[0]
+        end = keys[-1]
+
+        return (body['messages'][start][0], body['messages'][end][0])
+
+def decorate_start_end(file_name, start_end):
+    with open(file_name, 'r') as file1:
+        body = json.load(file1)
+        log_info = {
+            "start_time": start_end[0],
+            "end_time": start_end[1]
+        }
+        body["data"]["log_info"] = log_info
+        return body
+
+
+start_end = extract_start_end('output/0-frame.json')
+body = decorate_start_end('output/1-frame.json', start_end)
+with open('output/1-frame.json', 'w') as outfile:
+    json.dump(body, outfile)

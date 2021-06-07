f9d75bec70d3342b45af58e372739a75291e2a4c
curt <curt.k@kakaomobility.com>
2021-06-07 23:55:57 +0900

    hydra interpolation예제, list|dict 예제추가

--- hydra-first/step1/my_app.py
+++ hydra-first/step1/my_app.py
@@ -4,6 +4,13 @@ from omegaconf import DictConfig, OmegaConf
 @hydra.main(config_name="config")
 def my_app(cfg : DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
+    
+    print(cfg.db)
+    print(cfg.db.tables)
+    print(type(cfg.db.tables))
+    print(cfg.db.mappings)
+    print(type(cfg.db.mappings))
+
 
 if __name__ == "__main__":
     my_app()

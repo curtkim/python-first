d95c208fadb99e903a6d93ba97fcc4e2fd1a0294
curt <curt.k@kakaomobility.com>
2021-06-14 16:38:09 +0900

    minor

--- lang/testing_args.py
+++ lang/testing_args.py
@@ -2,15 +2,16 @@
 
 # 1) *argv
 def test_var_args(f_arg, *argv):
-    print("first normal arg:", f_arg)
-    for arg in argv:
-        print("another arg through *argv:", arg)
+    assert f_arg == 'yasoob'
+    assert argv == ('python', 'eggs', 'test')
 
-print(1)
 test_var_args('yasoob', 'python', 'eggs', 'test')
+
 
 # 2) **kwargs
 def greet_me(**kwargs):
+    assert kwargs == dict(name="yasoob", age=10)
+
     for key, value in kwargs.items():
         print("{0} = {1}".format(key, value))
 
@@ -24,11 +25,12 @@ def test_args_kwargs(arg1, arg2, arg3):
     print("arg2:", arg2)
     print("arg3:", arg3)
 
-print(3)
+
+print('tuple -> kwargs')
 args = ("two", 3, 5)
 test_args_kwargs(*args)
 
 # 4) with **kwargs
-print(4)
+print('dict -> kwargs')
 kwargs = {"arg3": 3, "arg2": "two", "arg1": 5}
 test_args_kwargs(**kwargs)

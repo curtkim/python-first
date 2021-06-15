364edaf374fcf5edb178714d08a577c99cb01158
curt <curt.k@kakaomobility.com>
2021-06-15 12:23:26 +0900

    hydar-first step4_multi_group에 string interpolation적용

--- hydra-first/step4_multi_group/my_app.py
+++ hydra-first/step4_multi_group/my_app.py
@@ -5,8 +5,7 @@ import hydra
 
 @hydra.main(config_path="conf", config_name="config")
 def my_app(cfg: DictConfig) -> None:
-    print(OmegaConf.to_yaml(cfg))
-
+    print(OmegaConf.to_yaml(cfg, resolve=True))
 
 if __name__ == "__main__":
     my_app()

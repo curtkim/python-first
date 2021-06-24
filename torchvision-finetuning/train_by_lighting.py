ba7e658af8c56082295a97a3e56fb1804a4bc281
curt <curt.k@kakaomobility.com>
2021-06-24 15:00:08 +0900

    torchvision-finetuning

--- torchvision-finetuning/train_by_lighting.py
+++ torchvision-finetuning/train_by_lighting.py
@@ -1,4 +1,5 @@
 import torch
+from typing import Tuple
 from utils.engine import train_one_epoch, evaluate
 import utils.utils as utils
 
@@ -19,22 +20,29 @@ class FudanLitModel(LightningModule):
 
     def training_step(self, batch, batch_idx):
         images, targets = batch
+        # print('batch_size', len(images), batch_idx)
+        # print('training_step', type(images), type(targets), batch_idx)
         loss_dict = self.model(images, targets)
+        # print(type(loss_dict))
         losses = sum(loss for loss in loss_dict.values())
         return {'loss': losses}
 
     def validation_step(self, batch, batch_idx):
         images, targets = batch
-        loss_dict = self.model(images, targets)
-        losses = sum(loss for loss in loss_dict.values())
-        return {'loss': losses}
+        # print('validation_step', type(images), type(targets), batch_idx, len(images))
+        result = self.model(images)
+        # print(result[0].keys())             # ['boxes', 'labels', 'scores', 'masks']
+        # print(result[0]['boxes'].shape)     # [100, 4]
+        # print(result[0]['labels'].shape)    # [100]
+        # print(result[0]['scores'].shape)    # [100]
+        # print(result[0]['masks'].shape)     # [100, 1, 363, 302]
+        #losses = sum(loss for loss in loss_dict.values())
+        #return {'loss': losses}
 
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(self.parameters(), lr=0.005,
                                     momentum=0.9, weight_decay=0.0005)
         return optimizer
-
-
 
 
 def main():
@@ -59,16 +67,18 @@ def main():
     data_loader = torch.utils.data.DataLoader(
         dataset, batch_size=2, shuffle=True, num_workers=4,
         collate_fn=utils.collate_fn)
+    print('data_loader len=', len(data_loader))
 
     data_loader_test = torch.utils.data.DataLoader(
         dataset_test, batch_size=1, shuffle=False, num_workers=4,
         collate_fn=utils.collate_fn)
+    print('data_loader_test len=', len(data_loader_test))
 
     # get the model using our helper function
     model = get_model_instance_segmentation(num_classes)
     litmodel = FudanLitModel(model)
 
-    trainer = Trainer(fast_dev_run=True) # , accelerator="ddp", gpus=2
+    trainer = Trainer(accelerator="ddp", gpus=2, max_epochs=13)    # ,fast_dev_run=True,
     trainer.fit(litmodel, data_loader, data_loader_test)
 
     torch.save(model.state_dict(), 'trained_lit.pth')

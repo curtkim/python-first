c9b10264dc3a5e80b43a9e4b965b1c5190e7f826
curt <curt.k@kakaomobility.com>
2021-06-26 14:05:45 +0900

    minor

--- torchvision/01_jpeg_load.py
+++ torchvision/01_jpeg_load.py
@@ -6,5 +6,6 @@ print(torchvision.__version__)
 
 data = read_file('car.jpg')  # raw data is on CPU
 img = decode_jpeg(data, device='cuda')  # decoded image in on GPU
-print(img.shape, img.dtype)
+print(type(img), img.shape, img.dtype)
+# <class 'torch.Tensor'> torch.Size([3, 2139, 3500]) torch.uint8
 

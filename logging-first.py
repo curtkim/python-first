55cc3e40bce298b8b49a65498c1df57f912bc8de
curt <curt.k@kakaomobility.com>
2021-09-20 12:38:48 +0900

    logging-first추가

---
+++ logging-first.py
@@ -0,0 +1,12 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
+logging.info('Useful message')

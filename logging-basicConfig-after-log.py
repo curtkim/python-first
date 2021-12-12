bf1498a40a62a3ed7a884e1b22e28ff66a15dce8
curt <curt.k@kakaomobility.com>
2021-12-12 12:40:38 +0900

    logging-basicConfig-after-log추가

---
+++ logging-basicConfig-after-log.py
@@ -0,0 +1,18 @@
+import logging
+
+logging.info('Useful message')
+
+# basicConfig전에 info를 호출한 경우에
+# 하나도 로깅인 남지 않는다.
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
+logging.info('variable %s', 'abc')
+logging.info('variable %d', 123)
+

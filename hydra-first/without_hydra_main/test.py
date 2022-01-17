9fbd037bfb5f787dd9c9efb2737462068b214d86
curt <curt.k@kakaomobility.com>
2022-01-17 16:28:16 +0900

    minor

--- hydra-first/without_hydra_main/test.py
+++ hydra-first/without_hydra_main/test.py
@@ -2,8 +2,21 @@ from hydra import compose, initialize
 from omegaconf import OmegaConf
 import os
 import sys
+from logging_tree import printout
+import logging
+
 
 if __name__ == '__main__':
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.StreamHandler()
+        ]
+    )
+
+    logging.info('start')
     initialize(config_path=".", job_name="test_app")
     cfg = compose(config_name="config", overrides=["db.user=me", "db.pass=pass"])
     print(OmegaConf.to_yaml(cfg))
@@ -12,4 +25,7 @@ if __name__ == '__main__':
     print('sys.argv=', sys.argv)
     cfg = compose(config_name="config", overrides=sys.argv[1:])
     print(OmegaConf.to_yaml(cfg))
+
+    #printout()
+    logging.info('end')
 

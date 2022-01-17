4be98dc90a888153e7be080530a8e52c0de71ad8
curt <curt.k@kakaomobility.com>
2022-01-17 16:21:53 +0900

    hydra without main수정

--- hydra-first/without_hydra_main/test.py
+++ hydra-first/without_hydra_main/test.py
@@ -1,9 +1,15 @@
 from hydra import compose, initialize
 from omegaconf import OmegaConf
 import os
+import sys
 
-initialize(config_path=".", job_name="test_app")
-cfg = compose(config_name="config", overrides=["db.user=me"])
-print(OmegaConf.to_yaml(cfg))
-print(os.getcwd())
+if __name__ == '__main__':
+    initialize(config_path=".", job_name="test_app")
+    cfg = compose(config_name="config", overrides=["db.user=me", "db.pass=pass"])
+    print(OmegaConf.to_yaml(cfg))
+    print('pwd=', os.getcwd())
+
+    print('sys.argv=', sys.argv)
+    cfg = compose(config_name="config", overrides=sys.argv[1:])
+    print(OmegaConf.to_yaml(cfg))
 

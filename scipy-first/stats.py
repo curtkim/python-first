d71e85c12250f7e5b5b8dd4e527d7d9fe700708c
curt <curt.k@kakaomobility.com>
2022-04-01 14:32:25 +0900

    scipy에 stats추가

---
+++ scipy-first/stats.py
@@ -0,0 +1,13 @@
+from scipy.stats import norm, poisson
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+r = norm.rvs(loc=0, scale =3, size=100)
+
+sns.distplot(r)
+plt.show()
+
+
+r = poisson.rvs(0.6, size=100)
+sns.distplot(r)
+plt.show()
